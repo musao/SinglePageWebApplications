7d391900afa57f0d11b212256e0c119a43406f63
Yuki Fujii <yuki-fujii@exa-corp.co.jp>
2025-10-26 20:08:13 +0900

    cp 3.6.3

--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -123,7 +123,11 @@ spa.shell = (function () {
 
     //---------------- イベントハンドラ開始 --------------------
     onClickChat = function ( event ) {
-        toggleChat( stateMap.is_chat_retracted );
+        if ( toggleChat( stateMap.is_chat_retracted ) ) {
+            $.uriAnchor.setAnchor({
+                chat : ( stateMap.is_chat_retracted ? 'open' : 'closed' )
+            });
+        }
         return false;
     };
     //---------------- イベントハンドラ終了 --------------------
